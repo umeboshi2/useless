a341a1b019c53f4f54295175cdbba2e17c4b1f7f
umeboshi <umeboshi@70758ab2-d2f7-0310-a994-9f7f813c4004>
2008-09-16 10:20:48 +0000

    removing dh_make example templates

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the useless package
-#
-0 4	* * *	root	useless_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(useless):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="useless" command="/usr/bin/useless"
